5fa097cc858386e807c4415cb3ddde3fddfcb9c3
Md. Adnan Abdullah <Adnanabdullah625@gmail.com>
2024-04-22 11:05:03 +0600

    commit 8

--- Unsocial Media/Extentions/Common Extension.swift
+++ Unsocial Media/Extentions/Common Extension.swift
@@ -8,6 +8,43 @@
 import Foundation
 import SwiftUI
 
+extension View {
+    func alert(isPresented: Binding<Bool>,
+               title: String,
+               message: String,
+               buttonText: String = "OK",
+               action: @escaping () -> Void = {}) -> some View {
+        alert(title,
+              isPresented: isPresented) {
+            Button(buttonText,
+                   action: action)
+        } message: {
+            Text(message)
+        }
+    }
+    
+    func alertWithOkAndCancel(isPresented: Binding<Bool>,
+                              title: String,
+                              message: String,
+                              buttonText: String = "OK",
+                              cancelButtonText: String = "Cancel",
+                              action: @escaping (Bool) -> Void) -> some View {
+        alert(title,
+              isPresented: isPresented) {
+            Button(buttonText) {
+                action(true)
+            }
+            Button(cancelButtonText) {
+                action(false)
+            }
+        } message: {
+            Text(message)
+        }
+    }
+}
+
+
+
 struct UnderlinedTextFieldStyle: TextFieldStyle {
     
     func _body(configuration: TextField<Self._Label>) -> some View {
